0507fb2a5c27514b527a29d9f8179e695b63a028
Lotuuuz <lanenabard1@gmail.com>
2026-05-14 19:52:22 +0200

    Animator Set-Up
    Animations, parameters and conditions Set-Up for MC Animations in Animator.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,50 +2,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  [SerializeField]  private float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed = 7f;
     private Rigidbody2D rb;
     private Vector2 movement;
     private Animator anim;
-    private SpriteRenderer sprite;
-
-    public bool isDoorInteraction = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-      
-
-        
 
         anim.SetFloat("Speed", Mathf.Abs(movement.x));
         anim.SetFloat("Direction", movement.x);
 
-        
+        // Start door interact ONCE
         if (Input.GetKeyDown(KeyCode.E))
         {
-            anim.SetBool("IsInteracting", true);
-            anim.SetBool("IsDoor", isDoorInteraction);
+            anim.SetBool("IsDoor", true);
         }
 
-        if (Input.GetKeyUp(KeyCode.E))
+        // Reset IsDoor when animation finishes
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (state.IsName("Interact_Door_Day") && state.normalizedTime >= 1f)
         {
-
-            anim.SetBool("IsInteracting", false);
-            anim.SetBool("IsDoor", false );
+            anim.SetBool("IsDoor", false);
         }
 
+        if (state.IsName("Interact_Door_Night") && state.normalizedTime >= 1f)
+        {
+            anim.SetBool("IsDoor", false);
+        }
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-
 }
