0304dcfd894be9df7d758e722c440fa2d509893f
Linram03 <ramsfjelllin@icloud.com>
2026-05-26 17:10:29 +0200

    Scripts for doors and keys
    Put in my own test scene, some test key sprites, and 3 scripts new scripts (Not done yet).

---
+++ Assets/Scripts/KeyManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+public class KeyManager : MonoBehaviour
+{
+  public static KeyManager instance;
+  
+  private HashSet<string> keys = new HashSet<string>();
+  
+  private void Awake()
+  {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+  }
+
+    public void AddKey(string keyName)
+    {
+        keys.Add(keyName);
+        Debug.Log("Picked up: " +  keyName);
+    }
+
+    public bool HasKey(string keyName)
+    {
+        return keys.Contains(keyName);
+    }
+}
---
+++ Assets/Scripts/KeyPickup.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class KeyPickup : MonoBehaviour
+{
+    public string keyName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            KeyManager.Instance.AddKey(keyName);
+
+            Destroy(gameObject);
+        }
+    }
+}
