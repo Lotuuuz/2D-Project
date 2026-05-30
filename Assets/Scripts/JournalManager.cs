d545949c6dd8570bb2081f49d3c00d6d01033b89
Lotuuuz <lanenabard1@gmail.com>
2026-05-30 14:59:49 +0200

    Journal UI updated

---
+++ Assets/Scripts/JournalManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class JournalManager : MonoBehaviour
+{
+    [Header("Main Panel")]
+    public GameObject journalPanel;
+
+    [Header("Backgrounds")]
+    public GameObject backgroundMenu;
+    public GameObject backgroundObjective;
+    public GameObject backgroundCharacter;
+
+    [Header("Pages")]
+    public GameObject menuPage;
+    public GameObject objectivePage;
+    public GameObject characterPage;
+
+    private bool isOpen = false;
+
+    void Start()
+    {
+        journalPanel.SetActive(false);
+        ShowMenuPage(); // Starter på meny-siden
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            ToggleJournal();
+        }
+    }
+
+    public void ToggleJournal()
+    {
+        isOpen = !isOpen;
+        journalPanel.SetActive(isOpen);
+    }
+
+    public void ShowMenuPage()
+    {
+        // Backgrounds
+        backgroundMenu.SetActive(true);
+        backgroundObjective.SetActive(false);
+        backgroundCharacter.SetActive(false);
+
+        // Pages
+        menuPage.SetActive(true);
+        objectivePage.SetActive(false);
+        characterPage.SetActive(false);
+    }
+
+    public void ShowObjectivePage()
+    {
+        backgroundMenu.SetActive(false);
+        backgroundObjective.SetActive(true);
+        backgroundCharacter.SetActive(false);
+
+        menuPage.SetActive(false);
+        objectivePage.SetActive(true);
+        characterPage.SetActive(false);
+    }
+
+    public void ShowCharacterPage()
+    {
+        backgroundMenu.SetActive(false);
+        backgroundObjective.SetActive(false);
+        backgroundCharacter.SetActive(true);
+
+        menuPage.SetActive(false);
+        objectivePage.SetActive(false);
+        characterPage.SetActive(true);
+    }
+}
+
