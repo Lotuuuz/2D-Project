a0875a0602aa44dc9a444c5727c3a97bc06257da
AlienDeity <jonadagsleth@gmail.com>
2026-05-31 14:31:48 +0200

    Sound managers added, and footsteps added to player when moving

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -4,13 +4,18 @@ public class MovementPlayer : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7.0f;
 
-
     public Rigidbody2D rb;
 
     private Vector2 movement;
 
     private Animator animator;
     public bool isFrozen = false;
+
+    [SerializeField] private AudioClip[] footstepSoundClips;
+
+    [SerializeField] private float footstepInterval = 1.0f;
+
+    private float footstepTimer;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -42,6 +47,18 @@ public class MovementPlayer : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (Mathf.Abs(movement.x) > 0.01f)
+        {
+            footstepTimer -= Time.fixedDeltaTime;
+
+            if (footstepTimer <= 0f)
+            {
+                SoundFXManager.Instance.PlayRandomSoundFXClip(footstepSoundClips, transform, 1f);
+
+                footstepTimer = footstepInterval;
+            }
+        }
     }
 
 }
