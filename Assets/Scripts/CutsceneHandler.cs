e4a2f516b25c16995e5ab7fdcc410c3d8765c537
Linram03 <ramsfjelllin@icloud.com>
2026-06-01 10:50:26 +0200

    scenefader cutscene
    Fixed the scene fader and worked a bit on cutscene scripts.

---
+++ Assets/Scripts/CutsceneHandler.cs
@@ -0,0 +1,25 @@
+using System.Transactions;
+using UnityEngine;
+
+public class CutsceneHandler : MonoBehaviour
+{
+    private CutsceneElementBase[] cutsceneElements;
+    private int index = -1;
+
+    public void Start()
+    {
+        cutsceneElements = GetComponentsInChildren<CutsceneElementBase>();
+    }
+
+    private void ExecuteCurrentElement()
+    {
+        if (index >= 0 && index < cutsceneElements.Length)
+            cutsceneElements[index].Execute();
+    }
+
+    public void PlayNextElement()
+    {
+        index++;
+        ExecuteCurrentElement();
+    }
+}
---
+++ Assets/Scripts/CutsceneInitiator.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CutsceneInitiator : MonoBehaviour
+{
+    private CutsceneHandler cutsceneHandler;
+
+    public void Start()
+    {
+        cutsceneHandler = GetComponent<CutsceneHandler>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+           cutsceneHandler.PlayNextElement();
+    }
+}
