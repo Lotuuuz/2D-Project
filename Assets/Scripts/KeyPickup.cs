a4c122ab5711adf218b5d789d7d0bc87bc60edd9
Linram03 <ramsfjelllin@icloud.com>
2026-06-02 17:53:49 +0200

    key pickup fix
    Press E key to pick up keys

--- Assets/Scripts/KeyPickup.cs
+++ Assets/Scripts/KeyPickup.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using TMPro;
 
 public class KeyPickup : MonoBehaviour
 {
     public string keyName;
 
+    [SerializeField] private GameObject pickupPrompt;
+
+    private bool playerInRange = false;
+
+    private void Start()
+    {
+        pickupPrompt.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            KeyManager.Instance.AddKey(keyName);
+
+            Debug.Log("Picked up:" + keyName);
+
+            pickupPrompt.SetActive(false);
+
+            Destroy(gameObject);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            KeyManager.Instance.AddKey(keyName);
+            playerInRange = true;
 
-            Destroy(gameObject);
+            pickupPrompt.SetActive(true);
+
+            Debug.Log("Press E to pick up key");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+
+            pickupPrompt.SetActive(false);
         }
     }
 }
