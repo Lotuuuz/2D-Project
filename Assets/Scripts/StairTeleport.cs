332164a98c6b4e3e9861e1c36032f443c16d5084
AlienDeity <jonadagsleth@gmail.com>
2026-06-02 15:26:10 +0200

    Sound Effects added to doors and stairs

--- Assets/Scripts/StairTeleport.cs
+++ Assets/Scripts/StairTeleport.cs
@@ -11,9 +11,9 @@ public class StairTeleport : MonoBehaviour
 
     [SerializeField] private float teleportDelay = 1.35f;
 
-    [SerializeField] private AudioClip[] doorSoundClips;
-
     [SerializeField] private AudioClip[] stairSoundClips;
+
+    [SerializeField] private AudioClip[] doorSoundClips;
 
 
     private bool playerInRange = false;
@@ -22,6 +22,10 @@ public class StairTeleport : MonoBehaviour
     private Animator playerAnimator;
 
     private bool isTeleporting = false;
+
+    public bool isDoor;
+
+    public bool isStair;
 
 
     // If the player is in the the trigger zone and presses e, they teleport
@@ -39,6 +43,16 @@ public class StairTeleport : MonoBehaviour
         isTeleporting = true;
 
         playerAnimator.SetTrigger("DoorInteract");
+
+        if (isDoor == true)
+        {
+            SoundFXManager.Instance.PlayRandomSoundFXClip(doorSoundClips, transform, 1f);
+        }
+
+        if (isStair == true)
+        {
+            SoundFXManager.Instance.PlayRandomSoundFXClip(stairSoundClips, transform, 1f);
+        }
 
         yield return new WaitForSeconds(teleportDelay);
 
