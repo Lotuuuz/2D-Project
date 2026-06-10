215b30a29b7a6b09b2402d5547127b929a064f70
Lotuuuz <lanenabard1@gmail.com>
2026-06-10 14:38:38 +0200

    Added all objectives to Night 1 Scene

--- Assets/Scripts/CompleteObjectiveOnEnter.cs
+++ Assets/Scripts/CompleteObjectiveOnEnter.cs
@@ -6,8 +6,17 @@ public class CompleteObjectiveOnEnter : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        if (ObjectiveManager.Instance.activeObjectives.Count < objectiveIndex + 1)
+        {
+          
+            return;
+
+        }
+
         if (collision.CompareTag("Player"))
         {
+          
             ObjectiveManager.Instance.CompleteObjective(objectiveIndex);
             Destroy(gameObject);
         }
